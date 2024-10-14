6d18b1dc4fe016348585440bc3dad2766b726005
Arush Singh <sarushmad@gmail.com>
2024-10-14 21:09:37 +0530

    updated fixed ecosystem config dist

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,14 +1,11 @@
-const { Script } = require("vm");
-
-module.exports = {
-    apps: [
-        {
-            name: "TaskVibe",
-            script: "npm",
-            args: "run dev",
-            env: {
-                NODE_ENV: "developement",
-            },
+export const apps = [
+    {
+        name: "TaskVibe",
+        script: "npm",
+        args: "run dev",
+        env: {
+            NODE_ENV: "developement",
         },
-    ],
-};
+    },
+];
+
